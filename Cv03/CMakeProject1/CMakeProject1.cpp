cb392f956db3d807122fed57cc186fe7d8be0880
Student <Student@PC-SE5113-22.utko.feec.vutbr.cz>
2024-10-02 08:29:41 +0200

    zmena

---
+++ Cv03/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,20 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+int main()
+{
+	float a;
+	double b;
+	double c;
+	b = 340.0;
+	printf("Napis cas:  ");
+	scanf("%f", &a);
+	c = a * b;
+	printf("Vysledek je %lf", c);
+		return 0;
+	
+}
---
+++ Cv03/vektory/vektory.cpp
@@ -0,0 +1,26 @@
+﻿// vektory.cpp : Defines the entry point for the application.
+//
+
+#include "vektory.h"
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	printf("Vektory 1: ");
+	scanf("%d" "%d", &a, &b);
+	printf("Vektory 2: ");
+	scanf("%d" "%d", &c, &d);
+
+	e = a + c;
+	f = b + d;
+
+	printf("Součet X: %d   Y:%d", e, f);
+	return 0;
+}
