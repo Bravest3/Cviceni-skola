a861d6d563eeac5d0272c5cbbd32edf3e7c246b8
Student <Student@PC-SE5113-22.utko.feec.vutbr.cz>
2024-10-09 07:40:45 +0200

    Pridano cviceni 4

--- Cv04/Cviceni 4/Cviceni 4.cpp
+++ Cv04/Cviceni 4/Cviceni 4.cpp
@@ -10,39 +10,72 @@ int jePrestupny(int rok)
 		
 		return 1;
 	}
-	else if ((rok % 100 != 0) && (rok % 4 == 0)) {
+	else if ((rok % 100 != 0 ) && (rok % 4 == 0)) {
 		return 1;
 	}
 	else {
 		return 0;
 	}
 	}
+int jeLichy(int rok)
+{
+	if (rok % 2 != 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+
+}
 int main()
 {
 	int a;
+	int g;
 	a = jePrestupny(1000);
-	
-	printf("1000   %d\n", a);
+	g = jeLichy(1000);
+	printf("1000     %d		%d\n", a,g);
 
 	int b;
+	int h;
 	b = jePrestupny(2000);
-	printf("2000   %d\n", b);
+	h = jeLichy(2000);
+	printf("2000	%d		%d\n", b,h);
 
 	int c;
+	int i;
 	c = jePrestupny(2002);
-	printf("2002   %d\n", c);
+	i = jeLichy(2002);
+	printf("2002   %d		%d\n", c,i);
 
 	int d;
+	int j;
 	d = jePrestupny(2012);
-	printf("2012   %d\n", d);
+	j = jeLichy(2012);
+	printf("2012   %d		%d\n", d,j);
 
 	int e;
+	int k;
 	e = jePrestupny(2022);
-	printf("2022   %d\n", e);
+	k = jeLichy(2022);
+	printf("2022   %d		%d\n", e,k);
 
 	int f;
+	int l;
 	f = jePrestupny(2200);
-	printf("2200   %d\n", f);
+	l = jeLichy(2200);
+	printf("2200   %d		%d\n", f,l);
 
+	int dalsi;
+	int dalsi1;
+	dalsi = jePrestupny(2201);
+	dalsi1 = jeLichy(2201);
+	printf("2201   %d		%d\n", dalsi, dalsi1);
+
+	int dalsi2;
+	int dalsi3;
+	dalsi2 = jePrestupny(2021);
+	dalsi3 = jeLichy(2021);
+	printf("2021   %d		%d\n", dalsi2, dalsi3);
+	
 	return 0;
 }
